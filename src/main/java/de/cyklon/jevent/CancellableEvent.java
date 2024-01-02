279f01999b3c94868337099853cf6ebbefc699bc
cyklon73 <cyklon698@gmail.com>
2024-01-02 18:46:55 +0100

    added documentation

--- src/main/java/de/cyklon/jevent/CancellableEvent.java
+++ src/main/java/de/cyklon/jevent/CancellableEvent.java
@@ -2,6 +2,36 @@ package de.cyklon.jevent;
 
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * This class provides an implementation for the Cancellable interface
+ * <p>
+ * Instead of
+ * <pre>{@code
+ * public class MyEvent extends Event implements Cancellable {
+ *   private boolean cancelled = false;
+ *
+ *                @Override
+ * 	public boolean isCancelled() {
+ * 		return cancelled;
+ * 	}
+ *
+ * 	@Override
+ * 	public void setCancelled(boolean cancelled) {
+ * 		this.cancelled = cancelled;
+ * 	}
+ * }
+ * }</pre>
+ *
+ * do
+ *
+ * <pre>{@code
+ * public class MyEvent extends CancellableEvent {
+ *
+ * }
+ * }</pre>
+ *
+ * @author <a href="https://github.com/cyklon73">Cyklon73</a>
+ */
 public abstract class CancellableEvent extends Event implements Cancellable {
 	private boolean cancelled = false;
 
