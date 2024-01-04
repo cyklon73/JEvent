c56b8c5d11a95208c33fc3077fd3b04203bbc8ca
cyklon73 <cyklon698@gmail.com>
2024-01-04 23:35:05 +0100

    implemented Handler base class

--- src/main/java/de/cyklon/jevent/Handler.java
+++ src/main/java/de/cyklon/jevent/Handler.java
@@ -2,79 +2,36 @@ package de.cyklon.jevent;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
- * The handler object represents a single event listener method
+ * The Handler Object is the base class for any type of handler
  *
  * @author <a href="https://github.com/cyklon73">Cyklon73</a>
  */
-class Handler implements Comparable<Handler> {
-	private final Object listener;
-	private final Method handler;
-	private Class<? extends Event> eventType = null;
-	private final byte priority;
-	private final boolean ignoreCancelled;
+abstract class Handler<T extends Event> implements Comparable<Handler<T>> {
+    protected Class<? extends Event> eventType;
+    private final byte priority;
+    private final boolean ignoreCancelled;
 
-	@SuppressWarnings("unchecked")
-	private Handler(@NotNull Object listener, @NotNull Method handler, byte priority, boolean ignoreCancelled) {
-		this.listener = listener;
-		this.handler = handler;
-		this.handler.setAccessible(true);
+    public Handler(Class<T> eventType, byte priority, boolean ignoreCancelled) {
+        this.eventType = eventType;
+        this.priority = priority;
+        this.ignoreCancelled = ignoreCancelled;
+    }
 
-		for(Class<?> parameterType : handler.getParameterTypes()) {
-			if(Event.class.isAssignableFrom(parameterType)) {
-				this.eventType = (Class<? extends Event>) parameterType;
-				break;
-			}
-		}
+    public boolean isSuitableHandler(@NotNull Class<? extends Event> event) {
+        return eventType.isAssignableFrom(event);
+    }
 
-		if(eventType == null) throw new EventException("the method must have an event as a parameter!");
-		this.priority = priority;
-		this.ignoreCancelled = ignoreCancelled;
-	}
+    @SuppressWarnings("unchecked")
+    public void invoke(@NotNull Event event) {
+        if(event instanceof Cancellable && ((Cancellable) event).isCancelled() && !ignoreCancelled) return;
+        if(isSuitableHandler(event.getClass())) invokeEvent((T) event);
+    }
 
-	@NotNull
-	public Object getListener() {
-		return listener;
-	}
+    protected abstract void invokeEvent(T event);
 
-	public boolean isSuitableHandler(@NotNull Class<? extends Event> event) {
-		return eventType.isAssignableFrom(event);
-	}
-
-	public void invoke(@NotNull Event event) {
-		if(event instanceof Cancellable && ((Cancellable) event).isCancelled() && !ignoreCancelled) return;
-		if(isSuitableHandler(event.getClass())) {
-			try {
-				handler.invoke(listener, event);
-			} catch(IllegalAccessException | InvocationTargetException e) {
-				throw new EventException(e);
-			}
-		}
-	}
-
-	@Override
-	public int compareTo(@NotNull Handler o) {
-		return Byte.compare(o.priority, this.priority);
-	}
-
-	@NotNull
-	public static Collection<Handler> getHandlers(@NotNull Object listener) {
-		Method[] methods = listener.getClass().getDeclaredMethods();
-		List<Handler> handlers = new LinkedList<>();
-
-		for(Method handler : methods) {
-			EventHandler annotation = handler.getAnnotation(EventHandler.class);
-			if(annotation == null) continue;
-
-			handlers.add(new Handler(listener, handler, annotation.priority(), annotation.ignoreCancelled()));
-		}
-
-		return handlers;
-	}
+    @Override
+    public int compareTo(@NotNull Handler o) {
+        return Byte.compare(o.priority, this.priority);
+    }
 }
