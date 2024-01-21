d4bdc514e61036f3fa108d2ee89f06ecb88e76eb
cyklon73 <cyklon698@gmail.com>
2024-01-21 19:45:11 +0100

    Configure Annotation

--- src/main/java/de/cyklon/jevent/ParameterInstance.java
+++ src/main/java/de/cyklon/jevent/ParameterInstance.java
@@ -2,9 +2,16 @@ package de.cyklon.jevent;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ParameterInstance {
 
     @NotNull
-    String name();
+    String value();
 
 }
